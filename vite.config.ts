6d5ac6deb6f49219717f3f18fc81891b9be0f84a
AliceAnnnc <aliceannnc@gmail.com>
2026-07-10 23:09:34 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  base: '/WeKongsi/',
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
